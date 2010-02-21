6f2a8fdbdbc78706e71985c61106fe24312d6a1b
lsb <leebutterman@gmail.com>
2010-02-21 10:21:22 -0500

    Read wikipedia pagewise, undo Wiki markup, and more.

---
+++ wp-words-to-db.rb
@@ -0,0 +1,60 @@
+require 'cgi'
+def w
+  IO.popen('bzip2 -cd enwiki-latest-pages-articles.xml.bz2') {|wpfile|
+    wpfile.each('</page>') {|page|
+      rawxml = page[/<page.+<\/page>$/m]
+      next unless rawxml
+      yield rawxml
+    }
+  }
+end
+def unmarkup(wtext)
+  CGI.unescapeHTML(wtext).
+      gsub(/<br ?\/?>/,"\n").gsub(/<!--.+?->/m,"").
+      gsub(/<(s|sup|sub|code|ref|references|includeonly|noinclude)[^<]+<\/\1>/m) {|tag| ''}.
+      gsub(/'{2,}/) {|emphasis| ''}.
+      gsub(/#REDIRECT \[\[.+?\]\]/) {|redirection|  '' }.
+      gsub(/~~~~|----/,'').tr('=','').
+      gsub(/\[\[[-a-z]+:[^\]]+\]\]/) {|category| ''}.
+      gsub(/\[\[(?:Category|Template):[^\]]+\]\]/) {|cat_templ|  ''}.
+      gsub(/\{[^\{\}]*\}/) {|metadata| ''}.gsub(/\{[^\{\}]*\}/) {|metadata| ''}.
+      gsub(/\{[^\{\}]*\}/) {|metadata| ''}.gsub(/\{[^\{\}]*\}/) {|metadata| ''}.
+      gsub(/\{[^\{\}]*\}/) {|metadata| ''}.gsub(/\{[^\{\}]*\}/) {|metadata| ''}.
+      gsub('{}','').
+      gsub(/\[\[([^\]|]+)\]\]/) {|ln| $1 }.
+      gsub(/\[\[[^\]\[]+\|([^\]\[]+)\]\]/) {|ln| $1 }.
+      gsub(/\[\[[^\]\[]+\|([^\]\[]+)\]\]/) {|ln| $1 }.
+      gsub(/\[\[[^\]\[]+\|([^\]\[]+)\]\]/) {|ln| $1 }.
+      gsub(/<[^>]+>/) {|tag| ''}.strip.gsub(/\n+/,"\n")
+end
+def unmarkedup
+  redirect = "<redirect />"
+  text = /<text.+<\/text>/m
+  w {|wpage|
+    #next if wpage.include?(redirect)
+    pagetext = wpage[text]
+    next if pagetext.nil?
+    unmarkedup = unmarkup(pagetext)
+    yield unmarkedup unless unmarkedup.empty?
+  }
+end
+def make_sql
+  word_id_autoincrement = 0
+  corpus_id_autoincrement = 0
+  word_id = Hash.new {|h,k| h[k] = word_id_autoincrement += 1 }
+  article_id = 0
+  puts "begin;"
+  unmarkedup {|text|
+    article_id += 1
+    text.downcase.scan(/[a-z]+/) {|word|
+      w = word_id[word]
+      corpus_id_autoincrement += 1
+      print "insert into corpus_words values (#{corpus_id_autoincrement},#{article_id},#{w});\n"
+      STDERR.print "." if corpus_id_autoincrement.%(10_000_000).zero?
+    }
+  }
+  STDERR.puts "corpus_words done"
+  puts "commit;begin;"  
+  word_id.sort_by {|k,v| v}.each {|k,v| print "insert into words values (#{v},'#{k}');\n" }
+  puts "commit;"
+end
